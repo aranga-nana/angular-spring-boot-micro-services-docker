3d693559c2671f9a34d5e137894046a340ab275a
Aranga C. Nanayakkara <aranga@outlook.com.au>
2016-02-09 21:35:31 +1100

    add comment

--- microservices/src/main/java/com/aranga/config/app/AppFileSystem.java
+++ microservices/src/main/java/com/aranga/config/app/AppFileSystem.java
@@ -1,6 +1,8 @@
 package com.aranga.config.app;
 
 /**
+ * Access to app home file system.
+ * can be use to store all the uploaded fiels
  * Created by Aranga on 7/02/2016.
  */
 public interface AppFileSystem
